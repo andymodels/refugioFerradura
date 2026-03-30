726aeeec56acd31023eaca60ebb8adf8c3955bfc
andymodels <joao19@gmail.com>
2026-03-30 14:58:22 +0000

    fix rota *

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -69,7 +69,7 @@ if (process.env.NODE_ENV === "production") {
     "artifacts/refugio-da-ferradura/dist/public",
   );
   app.use(express.static(staticDir));
-  app.get("*", (_req, res) => {
+  app.get("/*", (_req, res) => {
     res.sendFile(path.join(staticDir, "index.html"));
   });
 }
